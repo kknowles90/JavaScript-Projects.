338aaf20f788964d95d7637e25b0131f41c51da9
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-07-05 21:10:41 -0400

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var A= "this is a string";
+document.write(A);
+
+alert("Hello!");
+
+function My_First_Function(){
+    var str = "This text is green!";
+    var result = str.fontcolor ("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
