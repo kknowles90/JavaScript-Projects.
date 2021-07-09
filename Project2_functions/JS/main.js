6724e2a66edd431fd273b6619e042b383fd47f7f
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-07-09 16:47:50 -0400

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+
+function My_First_Function(){
+    var str = "This text is green!";
+    var result = str.fontcolor ("green");//this is the font color green
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var senctence = "I am learning";//this is a variable sentence
+    secntence += "a lot from this book!";//this is the addition to the variable
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
