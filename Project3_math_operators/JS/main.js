707e1fe139b224ab7ee9ff680b0914c68b574403
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-07-12 20:17:35 -0400

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,50 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML ="2 + 2 = " + addition; 
+}
+
+function subtraction_Function() {
+    var subtraction = 10 - 2;
+    document.getElementById("Math2").innerHTML ="10 - 2 =" + subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math3").innerHTML= "6 X 8 =" + simple_Math;
+}
+
+function division() {
+    var simple_Division = 48 / 6;
+    document.getElementById("Math4").innerHTML= "48 / 6 =" + simple_Division;
+}
+
+function more_Math() {
+    var simple_Math = (1+2) * 10 / 2 - 5;
+    document.getElementById("Math5").innerHTML= " 1 plus 2, multiplied by 10, divided in half and then subtacted by 5 equals " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of : " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math7").innerHTML = -x;
+}
+
+function increment_increase(){
+    var x = 5;
+    x++;
+    document.write(x);
+}
+
+function increment_decrease(){
+    var x = 5.25;
+    x--;
+    document.write(x);
+}
+
+function random(){
+    window.alert(Math.random() * 100);
+}
